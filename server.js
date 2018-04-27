133215695a94148f070d114018da54773f2f1da9
twesix <278227739@qq.com>
2018-04-27 08:34:04 +0800

    update lib and add question

--- server.js
+++ server.js
@@ -8,5 +8,13 @@ app.addQuestion
         question: '我希望我们的时间',
         tip: '永远停留在',
         answer: '八点二十六分'
-    },
+    }
+)
+app.addQuestion
+(
+    {
+        question: '我们的指挥官去哪儿了！？',
+        tip: '他哪儿也没去',
+        answer: '他只是长大了'
+    }
 )
